e415fa74865d75eaf718f7a254bff0f4f78d9e2b
Binary Banana <alpha.xt449.omega@gmail.com>
2024-12-13 23:19:06 -0500

    Add project files.

---
+++ EasyTcpClient.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MySocketLibrary
+{
+    internal class EasyTcpClient : IConnection
+    {
+        private readonly AsyncTcpClient client;
+        private readonly Encoding encoding;
+        private readonly int connectRetryTimeoutMs;
+        private readonly int connectRetryIntervalMs;
+
+        public event EventHandler StatusConnected
+        {
+            add => client.StatusConnected += value;
+            remove => client.StatusConnected -= value;
+        }
+
+        public event EventHandler StatusDisconnected
+        {
+            add => client.StatusDisconnected += value;
+            remove => client.StatusDisconnected -= value;
+        }
+
+        public event EventHandler<byte[]> DataReceivedAsBytes
+        {
+            add => client.DataReceived += value;
+            remove => client.DataReceived -= value;
+        }
+
+        public event EventHandler<string>? DataReceivedAsString;
+
+        public bool Connected
+        {
+            get => client.Connected;
+        }
+
+        /// <param name="encoding">Encoding to be used for <see cref="DataReceivedAsString"/> and <see cref="SendStringAsync(string)"/></param>
+        /// <param name="autoReconnect">Reconnect after <see cref="StatusDisconnected"/> is triggered</param>
+        /// <param name="connectRetryTimeoutMs">Duration that the socket will attempt to connect before timing out</param>
+        /// <param name="connectRetryIntervalMs">Interval that the socket will attempt to connect if the first attempt times out</param>
+        public EasyTcpClient(string host, int port, Encoding encoding, bool autoReconnect = true, int connectRetryTimeoutMs = 4_000, int connectRetryIntervalMs = 1_000)
+        {
+            client = new AsyncTcpClient(host, port);
+
+            client.DataReceived += Client_DataReceived;
+
+            if (autoReconnect)
+            {
+                client.StatusDisconnected += Client_Disconnected;
+            }
+
+            this.encoding = encoding;
+            this.connectRetryTimeoutMs = connectRetryTimeoutMs;
+            this.connectRetryIntervalMs = connectRetryIntervalMs;
+        }
+
+        /// <summary>
+        /// This can run forever if the server is not online and, therefore, is NOT awaitable.
+        /// Instead, use <see cref="StatusConnected"/>
+        /// </summary>
+        public async void ConnectAsync()
+        {
+            await client.ConnectAsync(new CancellationTokenSource(connectRetryTimeoutMs).Token);
+
+            // Retry loop
+            while (!client.Connected)
+            {
+                // Wait before attempting to connect again
+                await Task.Delay(connectRetryIntervalMs);
+
+                // Try again
+                await client.ConnectAsync(new CancellationTokenSource(connectRetryTimeoutMs).Token);
+            }
+        }
+
+        public void Disconnect()
+        {
+            client.Disconnect();
+        }
+
+        public async ValueTask SendBytesAsync(Memory<byte> data)
+        {
+            await client.SendAsync(data);
+        }
+
+        public async ValueTask SendStringAsync(string text)
+        {
+            await client.SendAsync(encoding.GetBytes(text));
+        }
+
+        // private
+
+        private void Client_Disconnected(object? sender, EventArgs args)
+        {
+            ConnectAsync();
+        }
+
+        private void Client_DataReceived(object? sender, byte[] data)
+        {
+            // Trigger event
+            DataReceivedAsString?.Invoke(this, encoding.GetString(data));
+        }
+    }
+}
---
+++ IConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MySocketLibrary
+{
+    public interface IConnection
+    {
+        public event EventHandler StatusConnected;
+        public event EventHandler StatusDisconnected;
+        public event EventHandler<byte[]> DataReceivedAsBytes;
+        public event EventHandler<string> DataReceivedAsString;
+
+        public bool Connected { get; }
+
+        public void ConnectAsync();
+
+        public void Disconnect();
+
+        public ValueTask SendBytesAsync(Memory<byte> buffer);
+
+        public ValueTask SendStringAsync(string text);
+    }
+}
