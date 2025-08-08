3eaf633bb61bf697bed8297bde9e2a5debc4f747
Jonathan Talcott <alpha.xt449.omega@gmail.com>
2025-08-08 12:03:00 -0400

    Added preliminary WebSocket client and connection types

---
+++ AsyncWebSocketClient.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyConnections;
+
+public class AsyncWebSocketClient
+{
+	private const int BUFFER_SIZE = 8192;
+
+	private readonly Uri uri;
+	private readonly HttpMessageInvoker? invoker;
+	private readonly byte[] inputBuffer;
+
+	private ClientWebSocket? webSocket;
+	private bool socketUsed;
+
+	public event EventHandler? StatusConnected;
+	public event EventHandler? StatusDisconnected;
+
+	public event EventHandler<byte[]>? BinaryReceived;
+	public event EventHandler<string>? TextReceived;
+
+	public bool Connected => webSocket?.State == WebSocketState.Open;
+
+	public AsyncWebSocketClient(Uri uri, HttpMessageInvoker? invoker = null)
+	{
+		this.uri = uri;
+		this.invoker = invoker;
+
+		inputBuffer = new byte[BUFFER_SIZE];
+	}
+
+	/// <summary>
+	/// This will disconnect and reconnect
+	/// </summary>
+	public async ValueTask ConnectAsync(CancellationToken cancellationToken)
+	{
+		// If socket initialized and has connected
+		if (webSocket != null && socketUsed)
+		{
+			// Dispose before reconnect
+			webSocket.Dispose();
+			webSocket = null;
+		}
+
+		// Initialize socket
+		webSocket = new ClientWebSocket();
+		socketUsed = false;
+
+		try
+		{
+			await webSocket.ConnectAsync(uri, invoker, cancellationToken);
+			socketUsed = true;
+		}
+		catch (SocketException)
+		{
+			// Ignore
+		}
+
+		// Socket successfully connected
+		if (socketUsed)
+		{
+			// Trigger event
+			StatusConnected?.Invoke(this, EventArgs.Empty);
+
+			// Begin receiving loop
+			StartReceivingData();
+		}
+	}
+
+	public void Disconnect()
+	{
+		// Already disconnected
+		if (webSocket == null)
+		{
+			return;
+		}
+
+		// Socket was connected and has not yet been disconnected
+		if (socketUsed)
+		{
+			webSocket.Dispose();
+			webSocket = null;
+
+			// Mark socket as being disconnected
+			socketUsed = false;
+
+			// Trigger event
+			StatusDisconnected?.Invoke(this, EventArgs.Empty);
+		}
+	}
+
+	public async ValueTask SendBinaryAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+	{
+		// Disconnected
+		if (webSocket == null)
+		{
+			return;
+		}
+
+		try
+		{
+			await webSocket.SendAsync(buffer, WebSocketMessageType.Binary, true, cancellationToken);
+		}
+		catch (SocketException)
+		{
+			// Ignore
+		}
+	}
+
+	public ValueTask SendTextAsync(string text, CancellationToken cancellationToken = default) => SendBinaryAsync(Encoding.UTF8.GetBytes(text).AsMemory(), cancellationToken);
+
+	// private
+
+	private async void StartReceivingData()
+	{
+		// Disconnected
+		if (webSocket == null)
+		{
+			return;
+		}
+
+		using (MemoryStream inputStream = new MemoryStream())
+		{
+			WebSocketReceiveResult result;
+
+			// Receive until end of message
+			do
+			{
+				result = await webSocket.ReceiveAsync(inputBuffer, CancellationToken.None);
+
+				// Handle disconnect
+				if (result.MessageType == WebSocketMessageType.Close)
+				{
+					Disconnect();
+					return;
+				}
+
+				// Write buffer to stream
+				inputStream.Write(inputBuffer, 0, result.Count);
+			}
+			while (!result.EndOfMessage);
+
+			if (result.MessageType == WebSocketMessageType.Binary)
+			{
+				BinaryReceived?.Invoke(this, inputStream.ToArray());
+			}
+			else if (result.MessageType == WebSocketMessageType.Text)
+			{
+				TextReceived?.Invoke(this, Encoding.UTF8.GetString(inputStream.ToArray()));
+			}
+		}
+
+		// Receive next data
+		StartReceivingData();
+	}
+}
---
+++ EasyWebSocketConnection.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyConnections;
+
+public class EasyWebSocketConnection : IConnection
+{
+	private readonly AsyncWebSocketClient client;
+	private readonly int connectRetryTimeoutMs;
+	private readonly int connectRetryIntervalMs;
+
+	public event EventHandler StatusConnected
+	{
+		add => client.StatusConnected += value;
+		remove => client.StatusConnected -= value;
+	}
+
+	public event EventHandler StatusDisconnected
+	{
+		add => client.StatusDisconnected += value;
+		remove => client.StatusDisconnected -= value;
+	}
+
+	public event EventHandler<byte[]>? DataReceivedAsBytes;
+
+	public event EventHandler<string> DataReceivedAsString
+	{
+		add => client.TextReceived += value;
+		remove => client.TextReceived -= value;
+	}
+
+	public bool Connected
+	{
+		get => client.Connected;
+	}
+
+	public Encoding Encoding { get; } = Encoding.UTF8;
+
+	public UsernamePasswordAuthentication Authentication { get; } = UsernamePasswordAuthentication.None;
+
+	/// <param name="authentication">May be used for authentication</param>
+	/// <param name="autoReconnect">Reconnect after <see cref="StatusDisconnected"/> is triggered</param>
+	/// <param name="connectRetryTimeoutMs">Duration that the socket will attempt to connect before timing out</param>
+	/// <param name="connectRetryIntervalMs">Interval that the socket will attempt to connect if the first attempt times out</param>
+	public EasyWebSocketConnection(Uri uri, HttpMessageInvoker? httpInvoker = null, bool autoReconnect = true, int connectRetryTimeoutMs = 4_000, int connectRetryIntervalMs = 1_000)
+	{
+		client = new AsyncWebSocketClient(uri, httpInvoker);
+
+		client.TextReceived += Client_TextReceived;
+
+		if (autoReconnect)
+		{
+			client.StatusDisconnected += Client_Disconnected;
+		}
+
+		this.connectRetryTimeoutMs = connectRetryTimeoutMs;
+		this.connectRetryIntervalMs = connectRetryIntervalMs;
+	}
+
+	public async Task ConnectAsync()
+	{
+		await client.ConnectAsync(new CancellationTokenSource(connectRetryTimeoutMs).Token);
+
+		// Retry loop
+		while (!client.Connected)
+		{
+			// Wait before attempting to connect again
+			await Task.Delay(connectRetryIntervalMs);
+
+			// Try again
+			await client.ConnectAsync(new CancellationTokenSource(connectRetryTimeoutMs).Token);
+		}
+	}
+
+	public void Disconnect() => client.Disconnect();
+
+	public async ValueTask SendBytesAsync(Memory<byte> data) => await client.SendTextAsync(Encoding.GetString(data.Span));
+
+	public async ValueTask SendStringAsync(string text) => await client.SendTextAsync(text);
+
+	// private
+
+	private void Client_Disconnected(object? sender, EventArgs args)
+	{
+		_ = ConnectAsync();
+	}
+
+	private void Client_TextReceived(object? sender, string text)
+	{
+		// Trigger event
+		DataReceivedAsBytes?.Invoke(this, Encoding.GetBytes(text));
+	}
+}
