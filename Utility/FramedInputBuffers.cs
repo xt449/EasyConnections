7dd15064184e37026f2363d45cbaa03422270e88
Jonathan Talcott <alpha.xt449.omega@gmail.com>
2025-08-29 13:50:27 -0400

    Added helper extensions methods for FramedInputBuffer types

---
+++ Utility/FramedInputBuffers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EasyConnections.Utility;
+
+public static class FramedInputBuffers
+{
+	public static void AddDelimiterFramedReceiveListener(this IDataIO io, Action<string> listener, params string[] delimiters)
+	{
+		new DelimiterFramedInputBuffer(io, delimiters).FrameReceived += (_, data) => listener(data);
+	}
+
+	internal static void AddRegexFramedReceiveListener(this IDataIO io, Action<Match> listener, params Regex[] regexes)
+	{
+		new RegexFramedInputBuffer(io, regexes).FrameReceived += (_, data) => listener(data);
+	}
+}
